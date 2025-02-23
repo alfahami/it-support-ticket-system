1677f26f0b4a8f747e8993ed734e13d89f9fcd0a
AL-FAHAMI <altoihir@gmail.com>
2025-02-23 19:40:27 +0000

    feat: add more validation rules

--- src/main/java/com/codelogium/ticketing/entity/User.java
+++ src/main/java/com/codelogium/ticketing/entity/User.java
@@ -12,7 +12,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,12 +30,15 @@ public class User {
     private Long id;
 
     @NotBlank(message = "Username cannot be null or blank")
+    @Size(min = 5, max = 8, message = "Username must be between 5 and 8 characters")
     private String username;
 
     @NotBlank(message = "Password cannot be null or blank")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
     @NotBlank(message = "Email cannot be null or blank")
+    @Email(message = "Invalid email format")
     private String email;
 
     private UserRole role;
