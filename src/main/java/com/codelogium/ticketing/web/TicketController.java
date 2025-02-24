af6c266561c68ceb50471672dc626735d38aa6ab
AL-FAHAMI <altoihir@gmail.com>
2025-02-24 09:04:59 +0000

    feat: add create ticket method handler

--- src/main/java/com/codelogium/ticketing/web/TicketController.java
+++ src/main/java/com/codelogium/ticketing/web/TicketController.java
@@ -1,18 +1,32 @@
 package com.codelogium.ticketing.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codelogium.ticketing.entity.Ticket;
 import com.codelogium.ticketing.service.TicketService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = "users/{userId}/tickets")
+@RequestMapping(value = "users/{userId}/tickets", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TicketController {
 
-    private TicketService TicketService;
+    private TicketService ticketService;
+
+    @PostMapping
+    public ResponseEntity<Ticket> createTicket(@PathVariable Long userId, @RequestBody @Valid Ticket newTicket) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ticketService.createTicket(userId, newTicket));
+    }
+
 
 
 }
