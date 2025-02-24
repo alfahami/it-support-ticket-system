353ec9f85db23236ee54b387b15bcf2e35634331
AL-FAHAMI <altoihir@gmail.com>
2025-02-24 08:24:05 +0000

    feat: add get user method handler

--- src/main/java/com/codelogium/ticketing/web/UserController.java
+++ src/main/java/com/codelogium/ticketing/web/UserController.java
@@ -1,8 +1,9 @@
 package com.codelogium.ticketing.web;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,11 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> registerUser(@RequestBody @Valid User user) {
         return  ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> retrieveUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.retrieveUser(userId));
     }
 
 
