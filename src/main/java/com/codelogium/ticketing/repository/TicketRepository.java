86331f438fc8aac3e6487cf961b15075d5dacd05
AL-FAHAMI <altoihir@gmail.com>
2025-02-24 18:35:32 +0000

    feat: find user by existing ticket

--- src/main/java/com/codelogium/ticketing/repository/TicketRepository.java
+++ src/main/java/com/codelogium/ticketing/repository/TicketRepository.java
@@ -4,10 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.codelogium.ticketing.entity.Ticket;
+import com.codelogium.ticketing.entity.User;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Optional<Ticket> findByIdAndCreatorId(Long ticketId, Long userId);
+
     List<Ticket> findByCreatorId(Long userId);
-} 
+
+    @Query("SELECT t.creator FROM Ticket t WHERE t.id = :ticketId")
+    Optional<User> findCreatorByTicket(@Param("ticketId") Long ticketId);
+}
