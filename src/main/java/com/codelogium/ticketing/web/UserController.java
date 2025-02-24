0d68aa68b9f8cb3b75edc5c58a398aae2b57c893
AL-FAHAMI <altoihir@gmail.com>
2025-02-24 08:34:59 +0000

    fix: correct use of uri id

--- src/main/java/com/codelogium/ticketing/web/UserController.java
+++ src/main/java/com/codelogium/ticketing/web/UserController.java
@@ -3,6 +3,7 @@ package com.codelogium.ticketing.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,12 @@ public class UserController {
     @GetMapping("/{userId}")
     public ResponseEntity<User> retrieveUser(@PathVariable Long userId) {
         return ResponseEntity.ok(userService.retrieveUser(userId));
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<HttpStatus> removeUser(@PathVariable Long userId) {
+        userService.removeUser(userId);
+        return ResponseEntity.noContent().build();
     }
 
 
