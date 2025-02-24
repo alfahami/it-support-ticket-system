a423e0deca6ad85e0b3b78a4494496187f3702fd
AL-FAHAMI <altoihir@gmail.com>
2025-02-24 09:00:24 +0000

    feat: add create ticket service method

--- src/main/java/com/codelogium/ticketing/service/TicketService.java
+++ src/main/java/com/codelogium/ticketing/service/TicketService.java
@@ -3,6 +3,6 @@ package com.codelogium.ticketing.service;
 import com.codelogium.ticketing.entity.Ticket;
 
 public interface TicketService {
-    Ticket createTicket(Ticket newTicket);
+    Ticket createTicket(Long userId, Ticket newTicket);
     
 }
--- src/main/java/com/codelogium/ticketing/service/TicketServiceImp.java
+++ src/main/java/com/codelogium/ticketing/service/TicketServiceImp.java
@@ -3,7 +3,9 @@ package com.codelogium.ticketing.service;
 import org.springframework.stereotype.Service;
 
 import com.codelogium.ticketing.entity.Ticket;
+import com.codelogium.ticketing.entity.User;
 import com.codelogium.ticketing.repository.TicketRepository;
+import com.codelogium.ticketing.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -12,9 +14,12 @@ import lombok.AllArgsConstructor;
 public class TicketServiceImp implements TicketService {
     
     private TicketRepository ticketRepository;
+    private UserRepository userRepository;
 
     @Override
-    public Ticket createTicket(Ticket newTicket) {
+    public Ticket createTicket(Long userId, Ticket newTicket) {
+       User user = UserServiceImp.unwrapUser(userId, userRepository.findById(userId));
+       newTicket.setCreator(user);
         return ticketRepository.save(newTicket);
     }
 }
