d999621229b9e9296ad512708928c9ae8fc27705
AL-FAHAMI <altoihir@gmail.com>
2025-02-25 02:21:21 +0000

    feat: use proper fk field to ensure relationship

--- src/main/java/com/codelogium/ticketing/repository/CommentRepository.java
+++ src/main/java/com/codelogium/ticketing/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.codelogium.ticketing.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<Comment> findByIdAndTicketIdAndCreatorId(Long commentId, Long ticketId, Long userId);
+    Optional<Comment> findByIdAndTicketIdAndAuthorId(Long commentId, Long ticketId, Long userId);
 }
