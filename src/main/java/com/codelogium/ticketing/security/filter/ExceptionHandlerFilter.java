ad5580ef7304646c10eb225cdc75d5e8774eb797
AL-FAHAMI <altoihir@gmail.com>
2025-02-25 21:33:30 +0000

    refractor: use helper to factor code

--- src/main/java/com/codelogium/ticketing/security/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/codelogium/ticketing/security/filter/ExceptionHandlerFilter.java
@@ -13,27 +13,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
-    
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
-        } catch(ResourceNotFoundException e) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);  
-            response.getWriter().write("Username doesn't exist");  
-            response.getWriter().flush();  
-        } catch(JWTVerificationException e) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);  
-            response.getWriter().write("JWT NOT VALID");  
-            response.getWriter().flush();
+        } catch (ResourceNotFoundException e) {
+            sendErrorResponse(response, HttpServletResponse.SC_NOT_FOUND, "Username doesn't exist");
+        } catch (JWTVerificationException e) {
+            sendErrorResponse(response, HttpServletResponse.SC_FORBIDDEN, "JWT NOT VALID");
+        } catch (RuntimeException e) {
+            sendErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, "BAD REQUEST");
         }
-        
-        catch (RuntimeException e) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);  
-            response.getWriter().write("BAD REQUEST");  
-            response.getWriter().flush();        
-        }
-        
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, int status, String message) throws IOException {
+        response.setStatus(status);
+        response.getWriter().write(message);
+        response.getWriter().flush();
     }
 }
