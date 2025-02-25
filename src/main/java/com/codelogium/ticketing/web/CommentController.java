4547f8366d8930d4c2abee88db48906a4e30680c
AL-FAHAMI <altoihir@gmail.com>
2025-02-25 02:36:55 +0000

    feat: implement retrieve comment handler

--- src/main/java/com/codelogium/ticketing/web/CommentController.java
+++ src/main/java/com/codelogium/ticketing/web/CommentController.java
@@ -3,6 +3,7 @@ package com.codelogium.ticketing.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,5 +31,10 @@ public class CommentController {
     @PatchMapping("/{commentId}")
     public ResponseEntity<Comment> updateComment(@PathVariable Long commentId, @PathVariable Long ticketId, @PathVariable Long userId, @RequestBody @Valid Comment newComment) {
         return ResponseEntity.ok(commentService.updateComment(commentId, ticketId, userId, newComment));
+    }
+
+    @GetMapping("/{commentId}")
+    public ResponseEntity<Comment> retrieveComment(@PathVariable Long commentId, @PathVariable Long ticketId, @PathVariable Long userId) {
+        return ResponseEntity.ok(commentService.retrieveComment(userId, ticketId, commentId));
     }
 }
