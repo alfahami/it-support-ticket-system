7fdfd8dd0806f36bd4ebab72f7bce8a83bfdb533
AL-FAHAMI <altoihir@gmail.com>
2025-02-25 10:41:25 +0000

    feat: add log ticket creation

--- src/main/java/com/codelogium/ticketing/service/CommentServiceImp.java
+++ src/main/java/com/codelogium/ticketing/service/CommentServiceImp.java
@@ -7,10 +7,12 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.codelogium.ticketing.entity.AuditLog;
 import com.codelogium.ticketing.entity.Comment;
 import com.codelogium.ticketing.entity.Ticket;
 import com.codelogium.ticketing.entity.User;
 import com.codelogium.ticketing.exception.ResourceNotFoundException;
+import com.codelogium.ticketing.repository.AuditLogRepository;
 import com.codelogium.ticketing.repository.CommentRepository;
 import com.codelogium.ticketing.repository.TicketRepository;
 
@@ -22,34 +24,50 @@ public class CommentServiceImp implements CommentService {
 
     private CommentRepository commentRepository;
     private TicketRepository ticketRepository;
+    private AuditLogRepository auditLogRepository;
 
     @Override
     public Comment createComment(Long ticketId, Long userId, Comment newComment) {
         User author = UserServiceImp.unwrapUser(userId, ticketRepository.findCreatorByTicket(ticketId));
 
-        Ticket retrieveTicket = TicketServiceImp.unwrapTicket(ticketId, ticketRepository.findByIdAndCreatorId(ticketId, userId));
+        Ticket retrieveTicket = TicketServiceImp.unwrapTicket(ticketId,
+                ticketRepository.findByIdAndCreatorId(ticketId, userId));
         newComment.setTicket(retrieveTicket);
         newComment.setAuthor(author);
         newComment.setTimestamp(Instant.now());
-        return commentRepository.save(newComment);
+        Comment createdComment = commentRepository.save(newComment);
+
+        // Log ticket creation
+        auditLogRepository.save(new AuditLog(
+                null,
+                createdComment.getId(),
+                userId,
+                "COMMENT_ADDED",
+                null,
+                null,
+                Instant.now()));
+
+        auditLogRepository.flush(); // Ensure immediate persistence
+
+        return createdComment;
     }
 
     @Override
     public Comment updateComment(Long commentId, Long ticketId, Long userId, Comment newComment) {
-        
+        // Verify user existance by checking the creator relationship
         UserServiceImp.unwrapUser(userId, ticketRepository.findCreatorByTicket(ticketId));
 
+        // Get the ticket and verify it belongs to this user
         TicketServiceImp.unwrapTicket(ticketId, ticketRepository.findByIdAndCreatorId(ticketId, userId));
 
-        Comment retrievedComment = unwrapComment(commentId, commentRepository.findByIdAndTicketIdAndAuthorId(commentId, ticketId, userId));
+        Comment retrievedComment = unwrapComment(commentId,
+                commentRepository.findByIdAndTicketIdAndAuthorId(commentId, ticketId, userId));
 
-        newComment.setId(retrievedComment.getId()); // prevent intentional ID tampering
-
-        //TODO: timestamp should be automatically changed when the user has made some changes
+        // TODO: timestamp should be automatically changed when the user has made some
+        // changes
         updateIfNotNull(retrievedComment::setAuthor, newComment.getAuthor());
         updateIfNotNull(retrievedComment::setContent, newComment.getContent());
-        updateIfNotNull(retrievedComment::setTicket
-        , newComment.getTicket());
+        updateIfNotNull(retrievedComment::setTicket, newComment.getTicket());
         updateIfNotNull(retrievedComment::setTimestamp, newComment.getTimestamp());
 
         return commentRepository.save(retrievedComment);
@@ -57,14 +75,15 @@ public class CommentServiceImp implements CommentService {
 
     @Override
     public Comment retrieveComment(Long userId, Long ticketId, Long commentId) {
-        // Checks if user exists
+        // Verify user existance by checking the creator relationship
         UserServiceImp.unwrapUser(userId, ticketRepository.findCreatorByTicket(ticketId));
 
-        // Checks if user has ticket
+        // Get the ticket and verify it belongs to this user
         TicketServiceImp.unwrapTicket(ticketId, ticketRepository.findByIdAndCreatorId(ticketId, userId));
 
-        // retrieves the comment of the checked user's ticket
-        Comment retrievedComment = unwrapComment(commentId, commentRepository.findByIdAndTicketIdAndAuthorId(commentId, ticketId, userId));
+        // Attempts to retrieve the corresponding comment
+        Comment retrievedComment = unwrapComment(commentId,
+                commentRepository.findByIdAndTicketIdAndAuthorId(commentId, ticketId, userId));
 
         return retrievedComment;
     }
@@ -75,7 +94,8 @@ public class CommentServiceImp implements CommentService {
 
         TicketServiceImp.unwrapTicket(ticketId, ticketRepository.findByIdAndCreatorId(ticketId, userId));
 
-        Comment retrievedComment = unwrapComment(commentId, commentRepository.findByIdAndTicketIdAndAuthorId(commentId, ticketId, userId));
+        Comment retrievedComment = unwrapComment(commentId,
+                commentRepository.findByIdAndTicketIdAndAuthorId(commentId, ticketId, userId));
 
         commentRepository.delete(retrievedComment);
     }
