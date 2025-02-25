b8068e44277901ee5b2c21f619ed0bef09e19195
AL-FAHAMI <altoihir@gmail.com>
2025-02-25 10:22:38 +0000

    feat: find all logs by ticket

---
+++ src/main/java/com/codelogium/ticketing/repository/AuditLogRepository.java
@@ -0,0 +1,11 @@
+package com.codelogium.ticketing.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codelogium.ticketing.entity.AuditLog;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long>{
+    List<AuditLog> findByTicketId(Long ticketId);
+}
