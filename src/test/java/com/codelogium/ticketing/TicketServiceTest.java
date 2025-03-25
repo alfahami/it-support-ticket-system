4c2dd27948dafd5e40d887b6b4a7a1b22f35787c
AL-FAHAMI <altoihir@gmail.com>
2025-03-25 23:42:35 +0000

    test: add ticket unit and end to end test base class

---
+++ src/test/java/com/codelogium/ticketing/TicketServiceTest.java
@@ -0,0 +1,9 @@
+package com.codelogium.ticketing;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class TicketServiceTest {
+    //TODO: implements Ticket Test with roles chekings
+}
