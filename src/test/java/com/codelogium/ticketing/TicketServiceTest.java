156224fbbc0928b72b1a734df1cd02a4d61fdcd8
AL-FAHAMI <altoihir@gmail.com>
2025-04-04 22:16:15 +0000

    test: implement retrieve ticket successfully

--- src/test/java/com/codelogium/ticketing/TicketServiceTest.java
+++ src/test/java/com/codelogium/ticketing/TicketServiceTest.java
@@ -50,6 +50,7 @@ public class TicketServiceTest {
         //Mock
         User testUser = new User(1L, "tupac", "tupac123", "tupac@gmail.com", UserRole.EMPLOYEE, null, null);
 
+        // Status is null in order to test if it being set while ticket saving
         Ticket ticket = new Ticket(1L, "Discrepancy while login", "Error 500 keeps pop up while password is correct", Instant.now(), null, Category.NETWORK, Priority.HIGH, testUser, null);
 
         when(userRepository.findById(1L)).thenReturn(Optional.of(testUser));
@@ -63,6 +64,26 @@ public class TicketServiceTest {
         assertEquals("Discrepancy while login", createdTicket.getTitle());
         assertEquals(Status.NEW, createdTicket.getStatus()); // Assure that status was set during ticket creation
         verify(ticketRepository, times(1)).save(any(Ticket.class));
+    }
+
+    @Test
+    void shouldRetrieveTicketSuccessfully() {
+        //Mock
+        User testUser = new User(1L, "tupac", "tupac123", "tupac@gmail.com", UserRole.EMPLOYEE, null, null);
+
+        // Status is null in order to test if it being set while ticket saving
+        Ticket ticket = new Ticket(1L, "Discrepancy while login", "Error 500 keeps pop up while password is correct", Instant.now(), null, Category.NETWORK, Priority.HIGH, testUser, null);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(testUser));
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(ticketRepository.findByIdAndCreatorId(1L, 1L)).thenReturn(Optional.of(ticket));
+
+        // Act
+        Ticket result = ticketService.retrieveTicket(1L, 1L);
+
+        // Assert
+        assertEquals(ticket.getId(), result.getId());
+        assertEquals(ticket.getTitle(), result.getTitle());
 
     }
 }
